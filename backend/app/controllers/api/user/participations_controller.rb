ecaa805617c1083ad616cc67f0efcec57d947bba
mkvtech <maksim.kulagin@stud.vilniustech.lt>
2023-03-27 16:37:20 +0300

    Fix user participations query

--- backend/app/controllers/api/user/participations_controller.rb
+++ backend/app/controllers/api/user/participations_controller.rb
@@ -3,7 +3,7 @@ module Api
     # :nodoc:
     class ParticipationsController < ApplicationController
       def index
-        @participations = Participation.includes(:event)
+        @participations = Participation.includes(:event).where(user: current_user)
         @participations = @participations.where(event_id: params[:event_id]) if params[:event_id].present?
       end
     end
