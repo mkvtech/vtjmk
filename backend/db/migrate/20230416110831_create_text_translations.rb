e7e84816058c779d89719c8824e990143f1c99be
mkvtech <maksim.kulagin@stud.vilniustech.lt>
2023-04-16 14:15:09 +0300

    BE: Add & install Mobility gem

---
+++ backend/db/migrate/20230416110831_create_text_translations.rb
@@ -0,0 +1,18 @@
+# rubocop:disable all
+# Disable rubocop for this file, since it was generated automatically
+
+class CreateTextTranslations < ActiveRecord::Migration[7.0]
+  def change
+    create_table :mobility_text_translations do |t|
+      t.string :locale, null: false
+      t.string :key,    null: false
+      t.text :value
+      t.references :translatable, polymorphic: true, index: false
+      t.timestamps null: false
+    end
+    add_index :mobility_text_translations, [:translatable_id, :translatable_type, :locale, :key], unique: true, name: :index_mobility_text_translations_on_keys
+    add_index :mobility_text_translations, [:translatable_id, :translatable_type, :key], name: :index_mobility_text_translations_on_translatable_attribute
+  end
+end
+
+# rubocop:enable all
