cb4cb0a61ca60fe1064a8932fd8d45367a9a0ee7
mkvtech <maksim.kulagin@stud.vilniustech.lt>
2023-05-02 17:52:38 +0300

    BE: Remove skipped tests & empty test files

--- backend/spec/models/event_spec.rb
+++ backend/spec/models/event_spec.rb
@@ -8,13 +8,6 @@ RSpec.describe Event do
       it { is_expected.to be_valid }
     end
 
-    # TODO: Rails does not work with enum validations, https://github.com/rails/rails/issues/13971
-    xcontext 'with invalid status' do
-      subject(:event) { build(:event, :with_conference, status: :invalid) }
-
-      it { is_expected.to be_invalid }
-    end
-
     context 'with invalid registration period' do
       subject(:event) do
         build(:event, :with_conference, registration_from: 5.days.from_now, registration_to: 5.days.ago)
