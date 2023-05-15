4d3fe1793fbc9a4b134466eaa5f7acb01837efa2
mkvtech <maksim.kulagin@stud.vilniustech.lt>
2023-05-15 12:13:12 +0300

    BE: Add filter to available_reviewers endpoint

--- backend/app/controllers/api/participations/users_controller.rb
+++ backend/app/controllers/api/participations/users_controller.rb
@@ -6,9 +6,15 @@ module Api
 
       # Lists available users to review participation request
       def available_reviewers_index
-        participation = Participation.includes(event: { event_reviewers: :reviewer }).find(params[:participation_id])
+        participation =
+          Participation
+          .includes(:reviews, event: { event_reviewers: :reviewer })
+          .find(params[:participation_id])
+
         authorize! participation, to: :update_reviewer?, with: ParticipationPolicy
-        @event_reviewers = participation.event.event_reviewers.where.not(reviewer_id: participation.user_id)
+
+        skip_user_ids = participation.reviews.map(&:user_id) + [participation.user_id]
+        @event_reviewers = participation.event.event_reviewers.where.not(reviewer_id: skip_user_ids)
       end
     end
   end
