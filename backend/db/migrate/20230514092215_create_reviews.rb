a23981b131b0c8465f3424c9d51cd3573edbbc82
mkvtech <maksim.kulagin@stud.vilniustech.lt>
2023-05-16 09:58:06 +0300

    BE: Fix status column nullable for Review

--- backend/db/migrate/20230514092215_create_reviews.rb
+++ backend/db/migrate/20230514092215_create_reviews.rb
@@ -1,7 +1,7 @@
 class CreateReviews < ActiveRecord::Migration[7.0]
   def change
     create_table :reviews do |t|
-      t.string :status, null: :false, default: :pending
+      t.string :status, null: false, default: :pending
       t.text :comment
       t.references :participation, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_15_195136) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_16_065633) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -158,7 +158,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_15_195136) do
   end
 
   create_table "reviews", force: :cascade do |t|
-    t.string "status", default: "pending"
+    t.string "status", default: "pending", null: false
     t.text "comment"
     t.bigint "participation_id", null: false
     t.bigint "user_id", null: false
