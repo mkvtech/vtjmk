cb02477676a06aebc79c14e7c4e2d1141f8a01e4
mkvtech <maksim.kulagin@stud.vilniustech.lt>
2023-05-17 11:25:46 +0300

    BE: Fix avatar url in different environments

--- backend/app/services/generate_avatar_url.rb
+++ backend/app/services/generate_avatar_url.rb
@@ -5,11 +5,6 @@ class GenerateAvatarUrl
   method_object %i[username!]
 
   def call
-    URI.join(root_url, letter_avatar_url(username, 64))
-  end
-
-  def root_url
-    # TODO: Use ENV['ROOT_URL']
-    'http://localhost:3000/'
+    letter_avatar_url(username, 64)
   end
 end
--- backend/config/initializers/letter_avatar.rb
+++ backend/config/initializers/letter_avatar.rb
@@ -1,7 +1,7 @@
 # https://github.com/ksz2k/letter_avatar
 LetterAvatar.setup do |config|
   # config.fill_color = 'rgba(255, 255, 255, 1)' # default is 'rgba(255, 255, 255, 0.65)'
-  config.cache_base_path = "public/system/letter_avatars/#{Rails.env}"
+  # config.cache_base_path = "public/system/letter_avatars/#{Rails.env}"
   # config.colors_palette = :iwanthue # default is :google
   config.weight = 500 # default is 300
   config.annotate_position = '-0+10' # default is -0+5
